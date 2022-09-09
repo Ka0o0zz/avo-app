0661b5a0a5a3f466e2286c17a78e3019413f0fd4
David Morales <davidbate5025@gmail.com>
2022-09-08 23:35:45 -0500

    aun no tenemos url de fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,11 @@ import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
 
 export async function getStaticProps() {
-  const response = await fetch('http://localhost:3000/api/avo')
-  const {data:productList }:TAPIAvoResponse = await response.json()
+  // const response = await fetch('http://localhost:3000/api/avo')
+  // const {data:productList }:TAPIAvoResponse = await response.json()
   return{
     props:{
-      productList
+      productList:[]
     }
   }
 }
